fc92f19a2d5f272f498de1911f485ec09702ea33
PatrykOlszewski1 <255668@edu.p.lodz.pl>
2026-03-19 21:13:46 +0100

    Poprawki do punktu 6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE_OF_NUMBERS = 12;
-    public static final int TOTAL_VALUE = -1;
-    private final int[] numbers = new int[SIZE_OF_NUMBERS];
+    public static final int CAPACITY_OF_NUMBERS = 12;
+    public static final int INITIAL_VALUE = -1;
+    public static final int RETURN_VALUE = -1;
+    public static final int DEFULT_VALUE = -1;
+    private final int[] numbers = new int[CAPACITY_OF_NUMBERS];
 
-    public int total = TOTAL_VALUE;
+    public int total = INITIAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == TOTAL_VALUE;
+        return total == DEFULT_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == CAPACITY_OF_NUMBERS -1;
     }
 
     protected int peekaBoo() {
         if (callCheck())
-            return TOTAL_VALUE;
+            return RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return TOTAL_VALUE;
+            return RETURN_VALUE;
         return numbers[total--];
     }
 
